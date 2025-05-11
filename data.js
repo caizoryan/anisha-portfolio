61941ab9da97e1c6214a2af4906ae890833dabfa
Aaryan Pashine <pashineaaryan@gmail.com>
2025-05-11 14:21:35 -0400

    updates

--- data.js
+++ data.js
@@ -2,21 +2,45 @@ export const data = [
   {
     "name": "CAT=DEAD",
     "images": [],
-    "videos": [],
+    "videos": [
+      "files/CAT=DEAD/AnishaVatnani_Anijam_Part6.mp4"
+    ],
     "description": "",
     "teaser": "files/CAT=DEAD/teaser.mp4"
   },
   {
     "name": "Korg",
-    "images": [],
+    "images": [
+      "files/Korg/04476ddb-3fbc-4ab3-8578-39358e4ef554_rw_1920.jpeg",
+      "files/Korg/114b4183-a3c2-47fb-82ff-b93bae7866c9_rw_1920.jpeg",
+      "files/Korg/909e9b63-4f62-44c1-9492-5669fada8905.jpg",
+      "files/Korg/Screenshot 2025-05-06 at 9.26.52 PM.png",
+      "files/Korg/Screenshot 2025-05-06 at 9.27.08 PM.png",
+      "files/Korg/Screenshot 2025-05-06 at 9.27.16 PM.png",
+      "files/Korg/Screenshot 2025-05-06 at 9.29.21 PM.png",
+      "files/Korg/ea5c4ba7-8529-4795-b29e-db79fa439023_rw_1920.jpeg",
+      "files/Korg/ec421fda-3974-4ccd-b43e-11d7cfd44bdf_rw_1920.jpeg",
+      "files/Korg/f1488aad-68a6-42d7-bd30-6d4053e073e9_rw_1920.jpeg"
+    ],
     "videos": [],
     "description": "",
     "teaser": "files/Korg/teaser.mp4"
   },
   {
     "name": "Māyā",
-    "images": [],
-    "videos": [],
+    "images": [
+      "files/Māyā/0082.png",
+      "files/Māyā/0614.png",
+      "files/Māyā/0651.png",
+      "files/Māyā/1631.png",
+      "files/Māyā/Untitled_Artwork 1.png",
+      "files/Māyā/Untitled_Artwork 4.png"
+    ],
+    "videos": [
+      "files/Māyā/IMG_6742.mp4",
+      "files/Māyā/IMG_6744.mp4",
+      "files/Māyā/IMG_6751.mp4"
+    ],
     "description": "",
     "teaser": "files/Māyā/teaser.mp4"
   },
@@ -29,14 +53,25 @@ export const data = [
   },
   {
     "name": "Robot - Modelling",
-    "images": [],
+    "images": [
+      "files/Robot - Modelling/848b1d4d-360a-4b46-b569-35d95d193e76_rw_1920.jpg",
+      "files/Robot - Modelling/8547137d-ccbb-45d8-9a4f-bf838c00cc85_rw_1920.jpg",
+      "files/Robot - Modelling/ba3e0380-5b40-4c70-8477-93b7254ea44c_rw_1920.jpg",
+      "files/Robot - Modelling/dd884959-7bb0-4f00-aeb3-ae8fcb651b5d_rw_1920.jpg"
+    ],
     "videos": [],
     "description": "",
     "teaser": "files/Robot - Modelling/teaser.mp4"
   },
   {
     "name": "Texturing - Robo",
-    "images": [],
+    "images": [
+      "files/Texturing - Robo/4d596b67-597a-46bf-b2ae-d9b09aaf2a81_rw_1920.jpg",
+      "files/Texturing - Robo/819ee27b-7c91-4255-8911-d35f82571162_rw_1200.jpg",
+      "files/Texturing - Robo/b9a421d0-cbc6-4f73-855a-9532db1b761c_rw_1920.jpg",
+      "files/Texturing - Robo/cd0c6e39-e39c-48ed-8ac6-8454f98496a0_rw_1920 (1).jpg",
+      "files/Texturing - Robo/d4fd19ec-25a4-4503-a736-ddaf5ff123a9_rw_1920.jpg"
+    ],
     "videos": [],
     "description": "",
     "teaser": "files/Texturing - Robo/teaser.mp4"
@@ -56,5 +91,19 @@ export const data = [
     ],
     "description": "",
     "teaser": "files/The Boiling Point/teaser.mp4"
+  },
+  {
+    "name": "texture_spider",
+    "images": [
+      "files/texture_spider/Render1.jpg",
+      "files/texture_spider/render2.jpg",
+      "files/texture_spider/render3.jpg",
+      "files/texture_spider/render4.jpg",
+      "files/texture_spider/render5.jpg",
+      "files/texture_spider/render6.jpg"
+    ],
+    "videos": [],
+    "description": "",
+    "teaser": "files/texture_spider/teaser.mp4"
   }
 ]
